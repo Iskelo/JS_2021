ab7b6970d09befef7d4aff42e054f35083e1ab0d
Iskelo <iskelo.kap@gmail.com>
2021-11-16 18:09:41 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/* const obj = {
+	name: 'John',
+	age: 25,
+	isMarried: false
+};
+
+console.log(obj.name); */
+
+const user = prompt("Jak masz na imie?", "");
+alert(`Cześć, ${user}!`);
+
+
+
